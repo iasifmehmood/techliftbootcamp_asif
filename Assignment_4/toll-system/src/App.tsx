0bf397e3a0c0c9507f04365c953536d8015ee0d5
asif-mehmood@hotmail.com <asif-mehmood@hotmail.com>
2022-11-02 17:03:55 +0500

    created table and icons

--- Assignment_4/toll-system/src/App.tsx
+++ Assignment_4/toll-system/src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 import "./App.css";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout></Layout>}>
+          <Route index element={<Home></Home>}></Route>
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
